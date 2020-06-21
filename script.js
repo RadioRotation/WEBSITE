786e2d9beb876a1906b45451fcb2cedf3277155e
RadioRotation <49866225+RadioRotation@users.noreply.github.com>
2020-06-21 15:18:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,20 @@ const divInstall = document.getElementById('installContainer');
 const butInstall = document.getElementById('butInstall');
 
 
-
+buttonInstall.addEventListener('click', (e) => {
+  // Hide the app provided install promotion
+  hideMyInstallPromotion();
+  // Show the install prompt
+  deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  deferredPrompt.userChoice.then((choiceResult) => {
+    if (choiceResult.outcome === 'accepted') {
+      console.log('User accepted the install prompt');
+    } else {
+      console.log('User dismissed the install prompt');
+    }
+  });
+});
 
 
 
