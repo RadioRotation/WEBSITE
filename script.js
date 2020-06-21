057b05b8db0c15d169022d54b12c8f54983df9bd
RadioRotation <49866225+RadioRotation@users.noreply.github.com>
2020-06-21 17:33:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,6 @@
-const divInstall = document.getElementById('installContainer');
-const butInstall = document.getElementById('butInstall');
 
-
-buttonInstall.addEventListener('click', (e) => {
-  // Hide the app provided install promotion
-  hideMyInstallPromotion();
-  // Show the install prompt
-  deferredPrompt.prompt();
-  // Wait for the user to respond to the prompt
-  deferredPrompt.userChoice.then((choiceResult) => {
-    if (choiceResult.outcome === 'accepted') {
-      console.log('User accepted the install prompt');
-    } else {
-      console.log('User dismissed the install prompt');
-    }
-  });
-});
-
-
-
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('https://raw.githubusercontent.com/RadioRotation/WEBSITE/master/service-worker.js');
+if('serviceWorker' in navigator) {
+  navigator.serviceWorker
+           .register('/service-worker.js')
+           .then(function() { console.log("Service Worker Registered"); });
 }
-
